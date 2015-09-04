94bcb3ddc15b2c2d95fa9d1f06c15556910088a8
Benoit Duffez <benoit@bicou.net>
2015-09-04 15:30:20 +0200

    Removed package name

--- ScriptRunner.java
+++ ScriptRunner.java
@@ -19,7 +19,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package io.tachy.db;
 
 import java.io.IOException;
 import java.io.LineNumberReader;
